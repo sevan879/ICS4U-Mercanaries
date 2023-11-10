fdbfd89a3539999e0d4db96047dca32eb3880602
Evan <revan879879@gmail.com>
2023-11-10 11:55:42 -0500

    Revert "transfered files from uncloned version to cloned version"
    This reverts commit 9fc9a4df78d35ff9305bebf4af98f10ba9899119.

--- ICS4U-Project2/MyWorld.java
+++ ICS4U-Project2/MyWorld.java
@@ -7,23 +7,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class MyWorld extends World
-{    
-    private Background b;
-    private Knight k;
-    
-    //constructor
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(543, 360, 1); 
-        b = new Background();
-        k = new Knight();
-        
-        addObject(b, 543, 180); //add background first, so its behind everything
-        addObject(k, 100, 305);
-    }
-    
-    //act method
-    public void act() {
+        super(600, 400, 1); 
     }
 }
