8b6570f4d3d977be8fe79806a1bc3ce4c345fe5b
Evan <revan879879@gmail.com>
2023-11-10 12:10:04 -0500

    Fix 1

--- ICS4U-Project2/MyWorld.java
+++ ICS4U-Project2/MyWorld.java
@@ -13,11 +13,13 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    Background b;
+    Knight k;
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        super(543, 360, 1); 
         b = new Background();
         k = new Knight();
         
@@ -43,13 +45,13 @@ public class MyWorld extends World
                 addObject(new Knight() , 0, getHeight()/2 );
             }
             else if(type == 1){
-                addObject(new Archer(), 0, getHeight()/2 );
+                //addObject(new Archer(), 0, getHeight()/2 );
             }
             else if(type == 2){
-                addObject(new , 0, getHeight()/2 );
+                //addObject(new , 0, getHeight()/2 );
             }
             else if(type == 3){
-                addObject(new , 0, getHeight()/2 );
+                //addObject(new , 0, getHeight()/2 );
             }
         }
     }
@@ -60,13 +62,13 @@ public class MyWorld extends World
                 addObject(new SkeletonKnight() , 0, getHeight()/2 );
             }
             else if(type == 1){
-                addObject(new SkeletonArcher(), 0, getHeight()/2 );
+                //addObject(new SkeletonArcher(), 0, getHeight()/2 );
             }
             else if(type == 2){
-                addObject(new SkeletonSpear(), 0, getHeight()/2 );
+                //addObject(new SkeletonSpear(), 0, getHeight()/2 );
             }
             else if(type == 3){
-                addObject(new SkeletonCrow(), 0, getHeight()/2 );
+                //addObject(new SkeletonCrow(), 0, getHeight()/2 );
             }
         }
     }
