885d819d7fb5933919ebdc4034c1585b4af309e1
Evan <revan879879@gmail.com>
2023-11-14 11:07:23 -0500

    My World update

--- ICS4U-Project2/MainWorld.java
+++ ICS4U-Project2/MainWorld.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class MainWorld extends World
 {    
     private Background b;
     private Knight k;
@@ -18,7 +18,7 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
+    public MainWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(543, 360, 1); 
