96c07e24f136520d609bbfcea14de08d36c05027
kennethj3 <117941586+kennethj3@users.noreply.github.com>
2023-11-16 20:26:25 -0500

    projectiles class

---
+++ ICS4U-Project2/Fireball.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Fireball here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fireball extends Projectiles
+{
+    private static final int speed = 10;
+    private static final int direction = 1;
+    public Fireball(){
+        super(speed, direction);
+    }
+    
+    /**
+     * Act - do whatever the Fireball wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
