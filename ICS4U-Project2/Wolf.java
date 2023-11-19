326eb9064f2fa5fcd1a6c9fd03588601695dba3e
kennethj3 <117941586+kennethj3@users.noreply.github.com>
2023-11-19 01:15:39 -0500

    wolves class

--- ICS4U-Project2/Wolf.java
+++ ICS4U-Project2/Wolf.java
@@ -8,6 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Wolf extends Enemy
 {
+    private static final int HP = 10;
+    private static final double SPEED = 5;
+    private static final int DELAY = 25;
+    private static final int DAMAGE = 2;
+    private static final int ATTACK_RANGE = 20;
+    private static boolean movable = true;
+    public Wolf(){
+        super(HP, SPEED, DELAY, DAMAGE, movable, ATTACK_RANGE);
+    }
+    
     /**
      * Act - do whatever the Wolf wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +25,13 @@ public class Wolf extends Enemy
     public void act()
     {
         // Add your action code here.
+    }
+    
+    protected void repelOtherEnemies(){
+        
+    }
+    
+    protected void action(Party targetPlayer){
+        
     }
 }
