a94c5e43f93e0fd2f736c525c683892ecc0ffd09
kennethj3 <349320036@gapps.yrdsb.ca>
2023-11-21 11:59:21 -0500

    fireball parabola motion

--- ICS4U-Project2/Fireball.java
+++ ICS4U-Project2/Fireball.java
@@ -8,10 +8,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Fireball extends Projectiles
 {
-    private static final int speed = 10;
+    private static final int speed = 2;
     private static final int direction = 1;
+    private static final int maxHeight = 150;
+    private static  int delay = 50;
     public Fireball(){
         super(speed, direction);
+        
     }
     
     /**
@@ -22,27 +25,86 @@ public class Fireball extends Projectiles
     {
         // Add your action code here.
         moveInArc();
+        
     }
     
+    /*
     private void moveInArc() {
-        int startX = getX();
-        int startY = getY();
-        int initialSpeed = speed;
+        //int startX = getX();
+        //int startY = getY();
+        int initialSpeed= speed;
         double radianAngle = Math.toRadians(45);
         int distanceMoved = 0;
-        while(distanceMoved <= 30){
-            System.out.println("movedup");
-            int x = (int) (startX + initialSpeed * Math.cos(radianAngle));
-            int y = (int) (startY - initialSpeed * Math.sin(radianAngle));
+        int x;
+        int y;
+        
+        while(initialSpeed > 0){
+            x = (int) (getX() + initialSpeed * Math.cos(radianAngle));
+            y = (int) (getY() - initialSpeed * Math.sin(radianAngle));
             setLocation(x, y);
-            distanceMoved += initialSpeed;
+            initialSpeed = initialSpeed - 1;
         }
-        int x = (int) (startX + initialSpeed * Math.cos(radianAngle));
-        int y = (int) (startY + initialSpeed * Math.sin(radianAngle));
+        x = (int) (getX() + initialSpeed * Math.cos(radianAngle));
+        y = (int) (getY() + initialSpeed * Math.sin(radianAngle));
         setLocation(x, y);
-
-        initialSpeed -= 1;
+        
     }
+    
+   
+   private void moveInArc(){
+       int velocityUp = speed;
+       if(velocityUp > 0){
+           setLocation(getX() + speed, getY() - speed/2);
+           velocityUp--;
+       }else{
+           setLocation(getX() + speed, getY() - speed/2);
+       }
+   }
+   
+   
+  
+  private void moveInArc(){
+      int actualSpeed = speed;
+      if(getY() > maxHeight){
+          
+          setLocation(getX() + speed, getY() - actualSpeed/2);
+      }else{
+          actualSpeed = actualSpeed * -1;
+          while(getY() < 330){
+              if(delay == 0){
+                  setLocation(getX() + speed, getY() - actualSpeed/2);
+                  delay = 50;
+              }else{
+                  delay--;
+              }
+              
+              if(getY() >= 330){
+                  break;
+              }
+          }
+          
+          
+      }
+  }
+  */
+  
+  boolean reachedMaxHeight = false;
+  private void moveInArc(){
+      
+      if(!reachedMaxHeight){
+          setLocation(getX() + speed, getY() - speed);
+      }else{
+          setLocation(getX() + speed, getY() + speed);
+      }
+      
+      if(getY() <= maxHeight){
+          reachedMaxHeight = true;
+
+      }
+      
+      
+  }
+  
 
     
     
