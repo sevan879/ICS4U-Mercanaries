fbebaa2803e704facf623e908d000c9790284cab
justinwuzijin <justinwu137@gmail.com>
2023-11-23 15:55:11 -0500

    added files

--- ICS4U-Project2/Wolf.java
+++ ICS4U-Project2/Wolf.java
@@ -1,67 +1,61 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import java.util.List;
 
 /**
  * Write a description of class Wolf here.
  * 
- * @author Justin Wu 
- * @version V0
+ * @author Justin Wu  
+ * @version (a version number or a date)
  */
 public class Wolf extends Enemy
 {
-    //imported from SkeletonSpear class
     private static final int HP = 5;
-    private static final double SPEED = 2;
-    private static final int DELAY = 30;
+    private static final double SPEED = 4;
+    private static final int DELAY = 10;
     private static final int DAMAGE = 2;
-    private static final int ATTACK_RANGE = 15;
+    private static final int ATTACK_RANGE = 95;
+    private static boolean movable = true;
 
-    private int animationTracker; // odd = running, even = not running
-    private int attackTracker; // 0, 1, 2 to decide which attack animation to use
     //Animation   
+    private GreenfootImage[] deathPics;
+    private int deathAnimationIndex;
+    private int deathAnimationDelay;
+    private int deathAnimationCounter;
+
     private GreenfootImage[] runningPics;
     private int runningAnimationIndex;
     private int runningAnimationDelay;
     private int runningAnimationCounter;
 
-    private GreenfootImage[] deadPics;
-    private int deadAnimationIndex;
-    private int deadAnimationDelay;
-    private int deadAnimationCounter;
+    private GreenfootImage[] attackOnePics;
+    private int attackOneAnimationIndex;
+    private int attackOneAnimationDelay;
+    private int attackOneAnimationCounter;
 
-    private GreenfootImage[] runningAttackPics;
-    private int runningAttackAnimationIndex;
-    private int runningAttackAnimationDelay;
-    private int runningAttackAnimationCounter;
-    
-    /**
-     * Act - do whatever the Wolf wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private GreenfootImage[] attackTwoPics;
+    private int attackTwoAnimationIndex;
+    private int attackTwoAnimationDelay;
+    private int attackTwoAnimationCounter;
+
+    private GreenfootImage[] attackThreePics;
+    private int attackThreeAnimationIndex;
+    private int attackThreeAnimationDelay;
+    private int attackThreeAnimationCounter;
+
+    //constructor
+    public Wolf() {
+        super(HP, SPEED, DELAY, DAMAGE, true, ATTACK_RANGE);
+        setImage(runningPics[1]);
+    }
+
+    //act method
     public void act()
     {
         super.act();
     }
-    
-    public Wolf(){
-        super(HP, SPEED, DELAY, DAMAGE, true, ATTACK_RANGE);
-        animationConstructor();
-    }
-    
-    protected void action(Party targetPlayer){
-        mainAttack();
-    }
 
-    private void mainAttack(){
+    protected void action(Party targetPlayer){
         if(playersUpClose() != null){
             for(Party p: playersUpClose()){
-                if (attackTracker == 0) { //attack one
-                    runningAttack();
-                    //stuff about dealing damage, whatever
-                    if (!animationIsRunning()) {
-                        attackTracker = 1;
-                    }
-                }
                 p.takeDamage(DAMAGE);
             }
         }
@@ -70,65 +64,38 @@ public class Wolf extends Enemy
             for(Party p: playersFurtherAway()){
                 p.takeDamage(DAMAGE - 1);
             }
-            runningAttack();
+        }
+    }
+
+    public void attackAnimation() {
+        if (attackTracker == 0) { //attack one
+            System.out.println("1");
+            attackOne();
             //stuff about dealing damage, whatever
             if (!animationIsRunning()) {
                 attackTracker = 1;
             }
         }
-    }
-
-    public void attackAnimation() {
-    }
-    
-    public void death() {
-    }
-    
-    public List<Party> playersUpClose(){
-        List<Party> fullDamage = getObjectsInRange(attackRange, Party.class);
-        return fullDamage;
-    }
-
-    public List<Party> playersFurtherAway(){
-        List<Party> halfDamage = getObjectsInRange(attackRange + 25, Party.class);
-        return halfDamage;
-    }
-
-    public void animationConstructor() {
-        animationTracker = 0;
-        attackTracker = 0;
-
-        //running
-        runningPics = new GreenfootImage[9];
-        for (int i = 0; i < runningPics.length; i++) {
-            runningPics[i] = new GreenfootImage("W0" + (i) + ".png");
-            runningPics[i].scale(runningPics[i].getWidth()*2, runningPics[i].getHeight()*2);
+        else if (attackTracker == 1) { //attack two
+            attackTwo();
+            System.out.println("2");
+            //stuff about dealing damage, whatever
+            if (!animationIsRunning()) {
+                attackTracker = 2;
+            }
         }
-        runningAnimationIndex = 0;
-        runningAnimationDelay = 10;
-        runningAnimationCounter = runningAnimationDelay;
+        else if (attackTracker == 2) { //attack three
+            attackThree();
+            System.out.println("3");
 
-        //dead 
-        deadPics = new GreenfootImage[2];
-        for (int i = 0; i < deadPics.length; i++) {
-            deadPics[i] = new GreenfootImage("W1" + (i) + ".png");
-            deadPics[i].scale(deadPics[i].getWidth()*2, deadPics[i].getHeight()*2);
+            //stuff about dealing damage, whatever, this one should deal the most since its the last attack
+            if (!animationIsRunning()) {
+                attackTracker = 0;
+            }
         }
-        deadAnimationIndex = 0;
-        deadAnimationDelay = 10;
-        deadAnimationCounter = deadAnimationDelay;
-
-        //running attack
-        runningAttackPics = new GreenfootImage[7];
-        for (int i = 0; i < runningAttackPics.length; i++) {
-            runningAttackPics[i] = new GreenfootImage("W2" + (i) + ".png");
-            runningAttackPics[i].scale(runningAttackPics[i].getWidth()*2, runningAttackPics[i].getHeight()*2);
-        }
-        runningAttackAnimationIndex = 0;
-        runningAttackAnimationDelay = 10;
-        runningAttackAnimationCounter = runningAttackAnimationDelay;
     }
 
+    //ANIMATION
     public void running() {
         if (runningAnimationCounter == 0){ // counter reaches 0 means ready for next frame
             runningAnimationCounter = runningAnimationDelay; // reset counter to max 
@@ -146,48 +113,161 @@ public class Wolf extends Enemy
         }
     }
 
-
-    public void dead() {
+    public void attackOne() {
         if (!animationIsRunning()) { //animationTracker is even, so we add one cuz we are starting animation
             animationTracker++;
         }
-        if (deadAnimationCounter == 0){ // counter reaches 0 means ready for next frame
-            deadAnimationCounter = deadAnimationDelay; // reset counter to max 
-            deadAnimationIndex++; // this will be used to set the image to the next frame
+        if (attackOneAnimationCounter == 0){ // counter reaches 0 means ready for next frame
+            attackOneAnimationCounter = attackOneAnimationDelay; // reset counter to max 
+            attackOneAnimationIndex++; // this will be used to set the image to the next frame
 
             // If the image index has passed the last image, stop animation
-            if (deadAnimationIndex == deadPics.length){
-                deadAnimationIndex = 0;
+            if (attackOneAnimationIndex == attackOnePics.length){
                 animationTracker++;
+                attackOneAnimationIndex = 0;
+            }
+            if (attackOneAnimationIndex == 3) {
+                setLocation(getX() - 20, getY()-9);
+            }
+            if (attackOneAnimationIndex == 4) {
+                setLocation(getX() + 20, getY()+9);
             }
             // Apply new image to this Actor
-            setImage (deadPics[deadAnimationIndex]);
+            setImage (attackOnePics[attackOneAnimationIndex]);
         } else {// not ready to animate yet, still waiting
             // so just decrement the counter          
-            deadAnimationCounter--;
+            attackOneAnimationCounter--;
         }
     }
 
-    public void runningAttack() {
+    public void attackTwo() {
         if (!animationIsRunning()) { //animationTracker is even, so we add one cuz we are starting animation
             animationTracker++;
         }
-        if (runningAttackAnimationCounter == 0){ // counter reaches 0 means ready for next frame
-            runningAttackAnimationCounter = runningAttackAnimationDelay; // reset counter to max 
-            runningAttackAnimationIndex++; // this will be used to set the image to the next frame
+        if (attackTwoAnimationCounter == 0){ // counter reaches 0 means ready for next frame
+            attackTwoAnimationCounter = attackTwoAnimationDelay; // reset counter to max 
+            attackTwoAnimationIndex++; // this will be used to set the image to the next frame
 
             // If the image index has passed the last image, stop animation
-            if (runningAttackAnimationIndex == runningAttackPics.length){
-                runningAttackAnimationIndex = 0;
+            if (attackTwoAnimationIndex == attackTwoPics.length){
                 animationTracker++;
+                attackTwoAnimationIndex = 0;
             }
+            // Apply new image to this Actor
+            if (attackTwoAnimationIndex == 3) {
+                setLocation(getX()-20, getY()-25);
+            }
+            if (attackTwoAnimationIndex == 5) {
+                setLocation(getX()+20, getY()+25);
+            }
+            setImage (attackTwoPics[attackTwoAnimationIndex]);
         } else {// not ready to animate yet, still waiting
             // so just decrement the counter          
-            runningAttackAnimationCounter--;
+            attackTwoAnimationCounter--;
         }
     }
 
-    public boolean animationIsRunning() {
-        return animationTracker %2 == 1; //this means that the animation is running, 
+    public void attackThree() {
+        if (!animationIsRunning()) { //animationTracker is even, so we add one cuz we are starting animation
+            animationTracker++;
+        }
+        if (attackThreeAnimationCounter == 0){ // counter reaches 0 means ready for next frame
+            attackThreeAnimationCounter = attackThreeAnimationDelay; // reset counter to max 
+            attackThreeAnimationIndex++; // this will be used to set the image to the next frame
+
+            // If the image index has passed the last image, stop animation
+            if (attackThreeAnimationIndex == attackThreePics.length){
+                animationTracker++;
+                attackThreeAnimationIndex = 0;
+            }
+            // Apply new image to this Actor
+            setImage (attackThreePics[attackThreeAnimationIndex]);
+
+        } else {// not ready to animate yet, still waiting
+            // so just decrement the counter          
+            attackThreeAnimationCounter--;
+        }
+    }
+
+    public void death() {
+        isDying = true;
+        boolean remove = false;
+        if (deathAnimationCounter == 0){ // counter reaches 0 means ready for next frame
+            deathAnimationCounter = deathAnimationDelay; // reset counter to max 
+            deathAnimationIndex++; // this will be used to set the image to the next frame
+
+            // If the image index has passed the last image, go back to first image
+            if (deathAnimationIndex == deathPics.length){
+                remove = true;
+                deathAnimationIndex = 0;
+            }
+            // Apply new image to this Actor
+            setLocation(getX(), getY()+5);
+            setImage (deathPics[deathAnimationIndex]);
+            if (deathAnimationIndex > 2) {
+                setLocation(getX(), getY()+25);
+            }
+        } else {// not ready to animate yet, still waiting
+            // so just decrement the counter          
+            deathAnimationCounter--;
+        }
+        if (remove) {
+            getWorld().removeObject(this);
+        }
+    }
+
+    protected void pickUp() {
+        movable = false;
+    }
+
+    public void animationConstructor() {
+        animationTracker = 0;
+        attackTracker = 0;
+        //death
+        deathPics = new GreenfootImage[4];
+        for (int i = 0; i < deathPics.length; i++) {
+            deathPics[i] = new GreenfootImage("SWD" + (i+1) + ".png");
+            deathPics[i].scale(deathPics[i].getWidth()*2, deathPics[i].getHeight()*2);
+        }
+        deathAnimationIndex = 0;
+        deathAnimationDelay = 10;
+        deathAnimationCounter = deathAnimationDelay;
+
+        //running
+        runningPics = new GreenfootImage[8];
+        for (int i = 0; i < runningPics.length; i++) {
+            runningPics[i] = new GreenfootImage("SWR" + (i+1) + ".png");
+            runningPics[i].scale(runningPics[i].getWidth()*2, runningPics[i].getHeight()*2);
+        }
+        runningAnimationIndex = 0;
+        runningAnimationDelay = 8;
+        runningAnimationCounter = runningAnimationDelay;
+
+        attackOnePics = new GreenfootImage[5];
+        for (int i = 0; i < attackOnePics.length; i++) {
+            attackOnePics[i] = new GreenfootImage("SWA1" + (i+1) + ".png");
+            attackOnePics[i].scale(attackOnePics[i].getWidth()*2, attackOnePics[i].getHeight()*2);
+        }
+        attackOneAnimationIndex = 0;
+        attackOneAnimationDelay = 8;
+        attackOneAnimationCounter = attackOneAnimationDelay;
+
+        attackTwoPics = new GreenfootImage[6];
+        for (int i = 0; i < attackTwoPics.length; i++) {
+            attackTwoPics[i] = new GreenfootImage("SWA2" + (i+1) + ".png");
+            attackTwoPics[i].scale(attackTwoPics[i].getWidth()*2, attackTwoPics[i].getHeight()*2);
+        }
+        attackTwoAnimationIndex = 0;
+        attackTwoAnimationDelay = 8;
+        attackTwoAnimationCounter = attackTwoAnimationDelay;
+
+        attackThreePics = new GreenfootImage[4];
+        for (int i = 0; i < attackThreePics.length; i++) {
+            attackThreePics[i] = new GreenfootImage("SWA3" + (i+1) + ".png");
+            attackThreePics[i].scale(attackThreePics[i].getWidth()*2, attackThreePics[i].getHeight()*2);
+        }
+        attackThreeAnimationIndex = 0;
+        attackThreeAnimationDelay = 8;
+        attackThreeAnimationCounter = attackThreeAnimationDelay;
     }
 }
