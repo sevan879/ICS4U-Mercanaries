e90f245cab8500ceb20824cffee1689356d5abd0
Evan <revan879879@gmail.com>
2023-11-23 21:47:22 -0500

    Finished merge

--- ICS4U-Project2/Healer.java
+++ ICS4U-Project2/Healer.java
@@ -11,7 +11,7 @@ public class Healer extends Party
     //BASE STATS AT LEVEL ONE
     private static final int SET_HP = 4;
     private static final double SET_SPEED = 3;
-    private static final int ACTION_DELAY = 30; // amount of acts
+    private static final int ACTION_DELAY = 120; // amount of acts
     private static final int XP_INCREASE_PER_LEVEL = 1;
     private static final int ATTACK_RANGE = 400;
     private static final int MAX_MANA = 100;
