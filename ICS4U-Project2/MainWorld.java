55a762022fbf1998633b3ee77336bb696cb93285
justinwuzijin <justinwu137@gmail.com>
2023-11-26 21:55:27 -0500

    final

--- ICS4U-Project2/MainWorld.java
+++ ICS4U-Project2/MainWorld.java
@@ -5,6 +5,113 @@ import java.util.*;
  * 
  * @author (your name) 
  * @version (a version number or a date)
+ * <head>
+ * <title> ICS4U Mercenaries </title>
+ * </head>
+ * 
+ * <p> This game is about a  mysterious wizard who is exiled from their village and 
+ * tries to take revenge by developing enemies to slowly kill off a village. 
+ * The village head takes notice and send a group of mercenaries to kill this mysterious being. 
+ * </p>
+ *  
+ * <h1>Assets:</h1>
+ * <p>
+ * https://craftpix.net/freebies/free-cartoon-forest-game-backgrounds/?num=1&count=85&sq=forest&pos=11
+ * <br>https://craftpix.net/freebies/free-rpg-battleground-asset-pack/
+ * <br>https://craftpix.net/freebies/free-villagers-sprite-sheets-pixel-art/?num=1&count=4&sq=villager&pos=0
+ * <br>https://craftpix.net/freebies/free-werewolf-sprite-sheets-pixel-art/?num=1&count=13&sq=wolf&pos=2
+ * <br>https://craftpix.net/freebies/free-werewolf-sprite-sheets-pixel-art/?num=1&count=13&sq=wolf&pos=2
+ * <br>https://www.youtube.com/watch?v=AIS8KbfVIcE
+ * <br>https://www.youtube.com/watch?v=k895zKWPEIg
+ * <br>https://www.youtube.com/watch?v=V77xa-SDI-I
+ * <br>https://elements.envato.com/large-fireball-special-effect-5TL4Z6Q
+ * <br>https://elements.envato.com/heal-64KWBLR
+ * <br>https://elements.envato.com/arrow-whoosh-bow-release-BXJTFKM
+ * <br>https://www.youtube.com/watch?v=1rwAvUvvQzQ
+ * <br>https://craftpix.net/freebies/free-skeleton-pixel-art-sprite-sheets/
+ * <br>https://craftpix.net/freebies/free-knight-character-sprites-pixel-art/
+ * <br>https://craftpix.net/freebies/free-wizard-sprite-sheets-pixel-art/
+ * <br>https://xyezawr.itch.io/gif-free-pixel-effects-pack-12-mini-magick-shoots-3
+ * <br>https://untiedgames.itch.io/super-pixel-effects-mini-pack-1?download
+ * </p>
+ * 
+ * <title>Key Features </title>
+ * <h1>MainWorld (gameplay loop)</h1>
+ * <p>
+ * //add
+ * </p>
+ * 
+ * <h1>Backstory</h1>
+ * <p>Pressing spacebar activates a new sentence spoken by either the villager or the knight about the situation at hand
+ * Uses Mr. Cohen’s SuperTextBox</p>
+ * 
+ * <h1>Settings</h1>
+ * <p>Controls the number of enemies and party members (party composition)
+ * Controls the difficulty of the enemy waves</p>
+ * 
+ * <h1>Entities</h1>
+ * <h2>Party</h2>
+ * <p>
+ * The mercenaries (good guys) that kill the enemies (bad guys)
+ * All party members have attack animations, death animations, idle animations, and running animations
+ * Mage, Dark Mage, and Healer consume mana. Knight does not.
+ * </p>
+ * <h3>PARTY TYPES</p3>
+ * <h4>Knight</p4>
+ * <p> Close quarters combat 
+ * The tank of the party (most hp, attracts enemy attacks)</p>
+ * <h4>Mage</h4>
+ * <p>Attacks with small or big fireball</p>
+ * <h4>Dark Mage</h4>
+ * <p>Same as mage, but with different attacks (different projectiles)</p>
+ * <h4>Healer</h4>
+ * <p>Heals party members, can either heal just one member or all of them</p>
+ * <h2>Enemy</h2>
+ * <p>All enemies have attack animations, death animations, and running animations</p>
+ * <h3>ENEMY TYPES</h3>
+ * <h4>Karel</h4>
+ * <p>Karel The Dog</p>
+ * <p>Burns when it dies</p>
+ * <h4>MiniCohen</h4>
+ * <p>Some sort of squid monster</p>
+ * <p>Custom made sprite</p>
+ * <p>Standard enemy otherwise</p>
+ * <h4>SkeletonArcher</h4>
+ * <p>Long range enemy type</p>
+ * <h4>SkeletonSpear</h4>
+ * <p>Close range, but can deal piercing damage</p>
+ * <h4>SkeletonWarrior</h4>
+ * <p>Standard enemy</p>
+ * <h4>Wolf</h4>
+ * <p>Standard enemy </p>
+ * <h2>Boss</h2>
+ * <h3>Mr. Cohen</h3>
+ * <p>Actions
+ * <br>MouthLaser: shoots a laser from mouth than does AOE damage to all party members
+ * <br>EyeLaser: shoots a deadly laser from eyes that kills of does 50% hp to one party member, chosen at random
+ * <br>Summon: summon either karel or minicohen
+ * <br>After a certain number of actions, Mr. Cohen goes down, this is when the party members attack and deal damage to him. 
+ * <br>After Mr. Cohen dies, the simulation ends
+ * <h2>Projectiles</h2>
+ * <h3>Arrow</h3>
+ * <p>Animations and sound for arrow flying through the sky using speed direction and velocity</p>
+ * <h3>Fireball</h3>
+ * <p>Explodes and gives off damage to enemies that get hit in range</p>
+ * <h3>smallFireball</h3>
+ * <p>Smaller version of fireball, but no splash damage is applied</p>
+ * <h2>Effects</h2>
+ * <h3>Explosion</h3>
+ * <p> //
+ * 
+ * 
+ * <h3>LoadingScreen</h3>
+ * <p>Fade in and fade out effect
+ * <br>Acts as a transition between worlds and background (main world levels)</p>
+ * <h2>Background</h2>
+ * <p>A giant image, endless scrolling (looping image)
+ * <br>Background image is based on what level </p>
+ * <h2>Button</h2>
+ * <p>Each button has its own image and function, determined in constructor and act method</p>
  */
 public class MainWorld extends World
 {    
